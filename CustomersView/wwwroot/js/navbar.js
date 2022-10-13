10071687b880abd86370b6bce7111050733ad48d
NguyenLocLinh <62052787+LocLinh@users.noreply.github.com>
2022-10-13 09:40:09 +0700

    create simple home page

---
+++ CustomersView/wwwroot/js/navbar.js
@@ -0,0 +1,12 @@
+﻿const searchBox = document.querySelector(".search-box");
+const searchBtn = document.querySelector(".search-btn");
+
+searchBtn.onclick = () => {
+    if (document.querySelector(".input-search").value.length == 0) {
+        document.querySelector(".input-search").focus();
+    }
+    else {
+        document.querySelector(".input-search").value = "";
+        // do search request here
+    }
+}
